3ed02b3559980ea03574c33cae59d9119e6703a4
merkyuri <kyuri.park@kakao.com>
2022-03-01 19:54:30 +0900

    Feat: add contribution point

--- src/extension.js
+++ src/extension.js
@@ -1,15 +1,14 @@
 const vscode = require("vscode");
+// const ERDEditorProvider = require("ERDEditorProvider");
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-  // console.log(
-  //   'Congratulations, your extension "erd-editor-schema-helper" is now active!'
-  // );
+  console.log("Congratulations, your extension is now active!");
 
   let disposable = vscode.commands.registerCommand(
-    "erd-editor-schema-helper.helloWorld",
+    "erd-editor-schema-helper.webview",
     function () {
       vscode.window.showInformationMessage(
         "Hello World from ERD Editor-Schema Helper!"
@@ -18,6 +17,12 @@ function activate(context) {
   );
 
   context.subscriptions.push(disposable);
+
+  // context.subscriptions.push(
+  //   vscode.commands.registerCommand("erd-editor-schema-helper.webview", () => {
+  //     ERDEditorProvider.createAndShow(context.extensionPath);
+  //   })
+  // );
 }
 
 function deactivate() {}
