575b32eb6c66f7c61aed430ecfffade4ee90de86
merkyuri <kyuri.park@kakao.com>
2022-03-15 14:55:21 +0900

    [ES-2] fixed command name & add extension icons (#7)
    * Feat: add contribution point
    
    * Feat: build custom editor with webview
    
    * Chore: command settings that are not called
    
    * Fix: issue of common JS - now fixed
    
    * Chore: add information message on command to create
    
    * Feat: add icons, Chore: rename command

--- src/extension.js
+++ src/extension.js
@@ -6,21 +6,35 @@ function activate(context) {
   console.log("Congratulations, your extension is now active!");
 
   context.subscriptions.push(
-    vscode.commands.registerCommand(
-      "erd-editor-schema-helper.helloworld",
-      () => {
-        vscode.window.showInformationMessage(
-          "Hello World from ERD Editor-Schema Helper!"
-        );
-      }
-    ),
-    vscode.commands.registerCommand("erd-editor-schema-helper.editor", () => {
-      DiagramEditorProvider.register(context);
+    vscode.commands.registerCommand("erdEditor.helloworld", () => {
+      vscode.window.showInformationMessage(
+        "Welcome to ERD Editor-Schema Helper extension."
+      );
     })
+  );
+
+  context.subscriptions.push(
+    // vscode.commands.registerCommand("erdEditor.webview", () => {
+    //   DiagramEditorProvider.register(context);
+
+    //   vscode.window
+    //     .showInformationMessage(
+    //       "Generated the ERD Editor with success!",
+    //       "Open"
+    //     )
+    //     .then((selected) => {
+    //       if (!selected) {
+    //         return;
+    //       }
+    //     });
+    // })
+    DiagramEditorProvider.register(context)
   );
 }
 
-function deactivate() {}
+function deactivate() {
+  console.info("Extension is deactivating.");
+}
 
 module.exports = {
   activate,
