6c40adec25a35192848677b8f1df78634821321c
merkyuri <kyuri.park@kakao.com>
2022-03-16 03:48:18 +0900

    Feat: add child_process & create write-stream

--- src/util.js
+++ src/util.js
@@ -18,11 +18,44 @@ function isOutputPanel(uri) {
   return uri.toString().startsWith("output:extension-output-");
 }
 
+function getErdTranslator(extensionId) {
+  const configuration = vscode.workspace.getConfiguration(extensionId);
+  const erdPath = configuration.get("erdPath");
+
+  if (erdPath === null || erdPath === undefined) {
+    return "erd";
+  } else {
+    return erdPath;
+  }
+}
+
+function getGraphviz(extensionId) {
+  const configuration = vscode.workspace.getConfiguration(extensionId);
+  const dotPath = configuration.get("dotPath");
+
+  if (dotPath === null || dotPath === undefined) {
+    return "dot";
+  } else {
+    return dotPath;
+  }
+}
+
+async function getSourceText(uri) {
+  const sourceUri = uri;
+  const sourceDocument = await vscode.workspace.openTextDocument(sourceUri);
+  const sourceText = sourceDocument.getText();
+
+  return sourceText;
+}
+
 let outputPanel = vscode.window.createOutputChannel("erdEditor");
 
 module.exports = {
   getNonce,
   isErdUri,
   isOutputPanel,
+  getErdTranslator,
+  getGraphviz,
+  getSourceText,
   outputPanel,
 };
