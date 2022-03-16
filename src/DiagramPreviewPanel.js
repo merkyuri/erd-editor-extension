e027cea032e2da191a038a1265a5fdc98e94ba7f
merkyuri <kyuri.park@kakao.com>
2022-03-16 15:37:07 +0900

    Feat: add process to show svg on the webview and add configuration (#13)

--- src/DiagramPreviewPanel.js
+++ src/DiagramPreviewPanel.js
@@ -1,11 +1,17 @@
 const path = require("path");
+const child_process = require("child_process");
 
 const vscode = require("vscode");
 
-const { getNonce } = require("./util.js");
+const {
+  getNonce,
+  getErdTranslator,
+  getGraphviz,
+  outputPanel,
+} = require("./util.js");
 
 class DiagramPreviewPanel {
-  static contentProviderKey = "erdEditor-schemaHelper";
+  static contentProviderKey = "erdEditor";
   _onDisposeEmitter = new vscode.EventEmitter();
   onDispose = this._onDisposeEmitter.event;
   _onDidChangeViewStateEmitter = new vscode.EventEmitter();
@@ -77,6 +83,24 @@ class DiagramPreviewPanel {
     this._panel.dispose();
   }
 
+  postMessage(message) {
+    if (this._panel.visible) {
+      this._panel.webview.postMessage(message);
+    } else {
+      this._postponedMessage = message; // saving the last update and flush it once panel become visible
+    }
+  }
+
+  async getUpdateWebViewMessage(uri) {
+    const document = await vscode.workspace.openTextDocument(uri);
+    const data = await this.convertToSvg(document.getText());
+
+    return updatePreview({
+      uri: uri.toString(),
+      data: data,
+    });
+  }
+
   setPanelIcon() {
     const root = path.join(this._extensionPath, "media");
 
@@ -86,12 +110,97 @@ class DiagramPreviewPanel {
     };
   }
 
-  postMessage(message) {
-    if (this._panel.visible) {
-      this._panel.webview.postMessage(message);
-    } else {
-      this._postponedMessage = message; // saving the last update and flush it once panel become visible
-    }
+  convertToSvg(erdContent) {
+    const erdTranslator = getErdTranslator("erdEditor");
+    const graphviz = getGraphviz("erdEditor");
+
+    return new Promise((resolve, reject) => {
+      outputPanel.clear();
+
+      const erdProcess = child_process.spawn(erdTranslator, ["-f", "dot"]);
+      const dotProcess = child_process.spawn(graphviz, ["-T", "svg"]);
+
+      let errorHandler = (commandName, error) => {
+        const codeProperty = "code";
+
+        if (error[codeProperty] === "ENOENT") {
+          outputPanel.clear();
+          outputPanel.append(`File not found: ${commandName} command`);
+          reject(new Error(`File not found: ${commandName} command`));
+        } else {
+          outputPanel.clear();
+          outputPanel.append(error.message);
+          reject(new Error(error.message));
+        }
+      };
+
+      erdProcess.on("error", (error) => errorHandler("erd", error));
+      dotProcess.on("error", (error) => errorHandler("dot", error));
+
+      try {
+        erdProcess.stdin.end(erdContent);
+        erdProcess.stdout.pipe(dotProcess.stdin);
+
+        let erdStdout = "";
+
+        erdProcess.stdout.on("data", (data) => {
+          if (data.toString().length > 0) {
+            erdStdout += data.toString();
+          }
+        });
+        erdProcess.on("close", (code) => {
+          if (code === 1) {
+            outputPanel.clear();
+            outputPanel.append(erdStdout);
+
+            let errorMessage = `
+              <tspan x="10" dy="1.2em">
+                  ERD file parse error
+              </tspan>
+              <tspan x="10" dy="1.2em"></tspan>
+            `;
+            erdStdout.split("\n").forEach((val) => {
+              errorMessage += `
+                <tspan x="10" dy="1.2em">
+                    ${this.escapeHtml(val)}
+                </tspan>
+              `;
+            });
+
+            let svgText = `
+              <svg class="textSVG" width="400pt" height="400pt" viewBox="0 0 400 400" xmlns="http://www.w3.org/2000/svg" style='stroke-width: 0px; background-color: white;'>
+                  <text font-size="15" x="0" y="10" fill="black">
+                    ${errorMessage}
+                  </text>
+              </svg>
+            `;
+
+            dotProcess.stdin.end();
+            resolve(svgText);
+          }
+        });
+
+        let svgText = "";
+
+        dotProcess.stdout.on("data", (data) => {
+          svgText += data.toString();
+        });
+
+        dotProcess.on("close", (code) => {
+          if (code === 0) {
+            resolve(svgText);
+          }
+        });
+      } catch (error) {
+        outputPanel.clear();
+        outputPanel.append(error);
+
+        erdProcess.kill("SIGKILL");
+        dotProcess.kill("SIGKILL");
+
+        reject(new Error(error));
+      }
+    });
   }
 
   getHtmlForWebview() {
@@ -133,6 +242,22 @@ class DiagramPreviewPanel {
     </html>
     `;
   }
+
+  escapeHtml(unsafe) {
+    return unsafe
+      .replace(/&/g, "&amp;")
+      .replace(/</g, "&lt;")
+      .replace(/>/g, "&gt;")
+      .replace(/"/g, "&quot;")
+      .replace(/'/g, "&#039;");
+  }
+}
+
+function updatePreview(payload) {
+  return {
+    command: "source:update",
+    payload,
+  };
 }
 
 module.exports = DiagramPreviewPanel;
